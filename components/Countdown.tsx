7ca5eac6c1e7d542f706457b6eb5c4a21b187dac
daancs <dadi.andrason@gmail.com>
2022-05-05 10:02:35 +0200

    added days

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 export default function Countdown() {
 
   const [timeLeft, setTimeLeft] = useState({
+    days: 0,
     hours: 0,
     minutes: 13,
     seconds: 37
@@ -15,11 +16,13 @@ export default function Countdown() {
 
     const msDelta: number = mottagningenStartDate.getTime() - todaysDate.getTime()
   
-    const hoursLeft: number = Math.floor(msDelta / 3_600_000)
+    const daysLeft: number = Math.floor(msDelta / (86_400_000))
+    const hoursLeft: number = Math.floor((msDelta % 86_400_000) / 3_600_000)
     const minutesLeft: number = Math.floor((msDelta % 3_600_000) / 60_000)
     const secondsLeft: number = Math.floor((msDelta % 60_000) / 1000)
 
     setTimeLeft({
+      days: daysLeft,
       hours: hoursLeft,
       minutes: minutesLeft,
       seconds: secondsLeft
@@ -31,7 +34,7 @@ export default function Countdown() {
     updateTimer()
     const downtick = setInterval(() => {
       updateTimer()
-      if (timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0) {
+      if (timeLeft.days === 0 && timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0) {
         clearInterval(downtick)
       }
     }, 1000)
@@ -39,7 +42,7 @@ export default function Countdown() {
 
   return (
     <div className="font-po absolute left-[20%] top-1/4 drop-shadow-md">
-      <div className="text-7xl drop-sh">{timeLeft.hours} : {timeLeft.minutes} : {timeLeft.seconds}</div>
+      <div className="text-7xl drop-sh">{timeLeft.days} : {timeLeft.hours} : {timeLeft.minutes} : {timeLeft.seconds}</div>
       <div className="text-4xl drop-sh">till mottagningen</div>
     </div>
   )
