13fac896f527b7b028a2d4d66d071d75d6a92eea
daancs <dadi.andrason@gmail.com>
2022-05-07 14:30:02 +0200

    added a description

--- pages/pateter.tsx
+++ pages/pateter.tsx
@@ -9,6 +9,16 @@ const Pateter: NextPage = () => {
                 <title>Pateter</title>
             </Head>
             <Page blackout={false} />
+            
+            <div className="patetdesc absolute left-[25%] top-[15%] drop-sh w-1/2 flex flex-col items-center">
+                <div className="text-5xl font-po mb-5">
+                    <h1>Pateter</h1>
+                </div>
+                <div className="font-light w-[75%] text-xl">
+                    På Chalmers är patet ett allmänt namn för personer som tidigare suttit i en förening/kommitté. De som har suttit i just NollKIT tidigare år kallas för NollQIT. De kan vara bra att ha lite då och då, både för NollKIT och för Nollan, eftersom de alltid svarar glatt på frågor om NollKIT råkar vara borta för stunden.
+                </div>
+            </div>
+
         </>
     )
 }
