ac965047105cb0898e31fc8d39ba5c21b408ae1c
daancs <dadi.andrason@gmail.com>
2022-05-07 21:18:02 +0200

    added patet component

---
+++ components/Patet.tsx
@@ -0,0 +1,13 @@
+import Page from "./Page";
+
+const Patet = ({ /*patet*/ }) => {
+    return (
+            <div className="patet-info">
+                <h1>not working pls</h1>
+            </div>
+    )
+}
+
+
+
+export default Patet;
