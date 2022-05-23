3529593abba46f02842d3175e0291de8af6eea09
Jacob <jacob.bengtsson@rocketmail.com>
2022-05-23 13:06:56 +0200

    Add epic redirect

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
         destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
         permanent: false,
       },
+      {
+        source: '/1337',
+        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        permanent: true,
+      },
     ]
   },
 }
