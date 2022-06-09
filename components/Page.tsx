1192058c8b1b4d385cf8fe55afcdaa5be2c7a01a
Jacob <jacob.bengtsson@rocketmail.com>
2022-06-09 23:18:03 +0200

    Render headers above everything

--- components/Page.tsx
+++ components/Page.tsx
@@ -12,7 +12,7 @@ export default function Page(props: PageProps) {
         <div className={`bg-cover bg-top w-full h-full ${props.blackout ? "opacity-60" : ""}`} style={{backgroundImage: "url('/bilder/bakgrund/2021.jpg')"}}></div>
       </div>
       {props.children}
-      <div className="fixed flex flex-col items-center w-screen top-0">
+      <div className="fixed flex flex-col items-center w-screen top-0 z-50">
         <Header blackout={props.blackout ?? false}/>
       </div>
     </>
