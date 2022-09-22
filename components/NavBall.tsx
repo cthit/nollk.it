13a94a12abd0d8bbe76f872f481eba3515c0fb72
Dadi Andrason <dadi.andrason@gmail.com>
2022-09-22 15:49:55 +0200

    updated css-class names to more intuitive names

--- components/NavBall.tsx
+++ components/NavBall.tsx
@@ -1,8 +1,8 @@
 const NavBall = (props: {index: number; committeeyear: string; currentPage: number; scrollTo: (to: number) => void }) => {
     return (
-        <div key={props.index} className="h-0 w-0 p-2.5 flex justify-end relative items-center m-px opacitykid">
-            <span className="transition-all opacity-0 numberop" key={props.index}>{props.committeeyear}</span>
-            <span key={props.index} onClick={() => { props.scrollTo(props.index) }} className={`floorButton ${props.index === props.currentPage ? 'bg-slate-100' : ''} border visible p-1.5 border-slate-100 opacity-50 hover:opacity-100 rounded-full m-2 transition-all`}></span>
+        <div key={props.index} className="h-0 w-0 p-2.5 flex justify-end relative items-center m-px navBallBox">
+            <span className="transition-all opacity-0 navBallLabel" key={props.index}>{props.committeeyear}</span>
+            <span key={props.index} onClick={() => { props.scrollTo(props.index) }} className={`navBall ${props.index === props.currentPage ? 'bg-slate-100' : ''} border visible p-1.5 border-slate-100 opacity-50 hover:opacity-100 rounded-full m-2 transition-all`}></span>
         </div>
     )
 };
