6f00e5228a74c708a6fa25141652db0375542f5e
Dadi Andrason <dadi.andrason@gmail.com>
2022-10-11 20:17:55 +0200

    fixed a cutoff on nollkit in hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -215,7 +215,7 @@ function YearSelector({defaultText, changeYear}: {defaultText: string, changeYea
   }, [])
   
   return (
-    <select className="px-0 lg:px-3 lg:font-light lg:text-xs lg:not-italic lg:my-0 text-xl italic mt-8 mb-3 bg-transparent outline-none appearance-none cursor-pointer" defaultValue={defaultText} onChange={e => { changeYear(e.target.value); e.target.selectedIndex = 0}}>
+    <select className="px-0 lg:px-3 lg:font-light lg:text-xs lg:not-italic lg:my-0 text-xl w-20 italic mt-8 mb-3 bg-transparent outline-none appearance-none cursor-pointer" defaultValue={defaultText} onChange={e => { changeYear(e.target.value); e.target.selectedIndex = 0}}>
       <option disabled hidden value={defaultText}>{defaultText}</option>
       {
         committees.map(committee => (
