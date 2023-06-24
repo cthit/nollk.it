ed7a72ff4a3a4b06cfa428409523de9b16208f1c
Jacob <jacob.bengtsson@rocketmail.com>
2023-06-24 18:17:11 +0200

    Update schema to match changes made in admin branch

--- pages/mottagningen.tsx
+++ pages/mottagningen.tsx
@@ -1,4 +1,4 @@
-import { Faq, PrismaClient } from "@prisma/client"
+import { PageText as PageTextType, Faq, PrismaClient } from "@prisma/client"
 import { NextPage } from "next"
 import { useContext } from "react"
 import ReactMarkdown from "react-markdown"
@@ -10,146 +10,145 @@ import PageText from "../components/PageText"
 import YearContext from "../util/YearContext"
 
 export const getServerSideProps = async () => {
-    const prisma = new PrismaClient()
+  const prisma = new PrismaClient()
 
-    const mottagningenText = (await prisma.pageText.findMany())[0].mottagningenText
-
-    const timeline = (await prisma.timeLineEvent.findMany(
-        {
-            include: {
-                category: true,
-                link: true
-            }
-        }
-    ))
-    const faqItems = (await prisma.faq.findMany());
-
-    const sortedFaqs = faqItems.sort((a, b) => a.orderInList - b.orderInList)
-
-    // dont know how this works but it works, sorts ascending (timeline order)
-    const sortedTimeline = timeline.sort(
-        (objA, objB) => new Date(objA.date).getTime() - new Date(objB.date).getTime(),
-    );
-
-    return {
-        props: {
-            mottagningenText: mottagningenText,
-            timelineData: sortedTimeline,
-            faqItems: sortedFaqs
-        }
+  const mottagningenText = (await prisma.pageText.findFirst({
+    where: {
+      page: "mottagningen"
     }
+  }))
+
+  const timeline = (await prisma.timeLineEvent.findMany(
+    {
+      include: {
+        category: true,
+        link: true
+      }
+    }
+  ))
+  const sortedTimeline = timeline.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+
+  const faqItems = await prisma.faq.findMany();
+  const sortedFaqs = faqItems.sort((a, b) => a.orderInList - b.orderInList)
+
+  return {
+    props: {
+      mottagningenText: mottagningenText,
+      timelineData: sortedTimeline,
+      faqItems: sortedFaqs
+    }
+  }
 }
 
 const parseDateTime = (dateString: string) => {
-    const date = new Date(dateString)
-    const day = date.getDate()
-    const month = date.toLocaleString('default', { month: 'long' })
-    const year = date.getFullYear()
-    const parsedDate = `${day} ${month} ${year}`
-    return parsedDate
+  const date = new Date(dateString)
+  const day = date.getDate()
+  const month = date.toLocaleString('default', { month: 'long' })
+  const year = date.getFullYear()
+  const parsedDate = `${day} ${month} ${year}`
+  return parsedDate
 }
 
 interface MottagningenProps {
-    mottagningenText: string
-    timelineData: timelineData[]
-    faqItems: Faq[]
+  mottagningenText: PageTextType
+  timelineData: TimelineData[]
+  faqItems: Faq[]
 }
 
 
-interface timelineData {
-    text: string,
-    year: string,
-    date: string,
-    category: {
-        title: string,
-        color: string,
-    },
-    link?: {
-        url: string,
-        name: string,
-    }
+interface TimelineData {
+  text: string,
+  year: string,
+  date: string,
+  category: {
+    title: string,
+    color: string,
+  },
+  link?: {
+    url: string,
+    name: string,
+  }
 }
 
 
-const TimelineItem = ({ data }: { data: timelineData }) => (
-    <div className="timeline-item">
-        <div className="timeline-item-content">
-            <span className="tag px-3 py-1 top-3 left-3" style={{ background: data.category.color }}>
-                {data.category.title}
-            </span>
-            <time>{parseDateTime(data.date)}</time>
-            <div className="my-4 text-left self-start">
-                <ReactMarkdown children={data.text}></ReactMarkdown>
-            </div>
-            {data.link && (
-                <a
-                    href={data.link.url}
-                    target="_blank"
-                >
-                    {data.link.name}
-                </a>
-            )}
-            <span className="circle" />
-        </div>
+const TimelineItem = ({ data }: { data: TimelineData }) => (
+  <div className="timeline-item">
+    <div className="timeline-item-content">
+      <span className="tag px-3 py-1 top-3 left-3" style={{ background: data.category.color }}>
+        {data.category.title}
+      </span>
+      <time>{parseDateTime(data.date)}</time>
+      <div className="my-4 text-left self-start">
+        <ReactMarkdown children={data.text}></ReactMarkdown>
+      </div>
+      {data.link && (
+        <a
+          href={data.link.url}
+          target="_blank"
+        >
+          {data.link.name}
+        </a>
+      )}
+      <span className="circle" />
     </div>
+  </div>
 );
 
-const Timeline = ({ data }: { data: timelineData[] }) => {
-    if (data.length > 0) {
-        return (
-            <div className="timeline-container">
-                {data.map((data, idx) => (
-                    <TimelineItem data={data} key={idx} />
-                ))}
-            </div>
-        )
-    } else {
-        return (
-            <>
-            </>
-        );
-    }
+const Timeline = ({ data }: { data: TimelineData[] }) => {
+  if (data.length > 0) {
+    return (
+      <div className="timeline-container">
+        {data.map((data, index) => (
+          <TimelineItem data={data} key={index} />
+        ))}
+      </div>
+    )
+  } else {
+    return <></>
+  }
 }
 
 
 const Mottagningen: NextPage<MottagningenProps> = ({ mottagningenText, timelineData, faqItems }) => {
 
-    const year = useContext(YearContext).year
-    const filteredTimelineData = timelineData.filter((data) => data.year === year)
+  const year = useContext(YearContext).year
+  const filteredTimelineData = timelineData.filter((data) => data.year === year)
 
-    return (
-        <>
-            <Page blackout>
+  return (
+    <>
+      <Page blackout>
 
-                <PageInfo heading="Hej Nollan!">
-                    {mottagningenText}
-                </PageInfo>
-                <Divider />
-                <div className="timeline-box mt-[20vh] flex flex-col items-center">
-                    <div className="font-bold text-6xl font-po text-center mb-12">Timeline</div>
+        <PageInfo heading="Hej Nollan!">
+          {mottagningenText.content}
+        </PageInfo>
 
-                    <div className="mb-12">
-                        <PageText>Nedan finns en timeline över allting man kan behöva göra innan mottagningen samt deadlines till dessa. Timelinen kan även innehålla lite roliga grejer som även sker efter mottagningen såsom aspning!</PageText>
-                    </div>
-                    <Timeline data={filteredTimelineData} />
-                </div>
+        <Divider />
 
-                <Divider />
+        <div className="timeline-box mt-[20vh] flex flex-col items-center">
+          <div className="font-bold text-6xl font-po text-center mb-12">Timeline</div>
 
-                <div className="font-po font-bold w-[60vw] mt-8 ml-4">
-                    <span className="text-4xl">Frequently Asked Questions</span>
-                </div>
-                <div>
-                    {faqItems.map((faqItem, index) => (
-                        <div>
-                            <Accordion title={faqItem.question} key={index} content={faqItem.answer} />
-                        </div>
-                    ))}
-                </div>
+          <div className="mb-12">
+            <PageText>Nedan finns en timeline över allting man kan behöva göra innan mottagningen samt deadlines till dessa. Timelinen kan även innehålla lite roliga grejer som även sker efter mottagningen såsom aspning!</PageText>
+          </div>
+          <Timeline data={filteredTimelineData} />
+        </div>
 
-            </Page>
-        </>
-    )
+        <Divider />
+
+        <div className="font-po font-bold w-[60vw] mt-8 ml-4">
+          <span className="text-4xl">Frequently Asked Questions</span>
+        </div>
+        <div>
+          {faqItems.map((faqItem, index) => (
+            <div key={index}>
+              <Accordion title={faqItem.question} content={faqItem.answer} />
+            </div>
+          ))}
+        </div>
+
+      </Page>
+    </>
+  )
 }
 
 
