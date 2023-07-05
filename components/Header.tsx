60d25808edfa51dfee256ee6c05d1ac73d3d3f23
Jacob <jacob.bengtsson@rocketmail.com>
2023-07-05 20:12:00 +0200

    fix images

--- components/Header.tsx
+++ components/Header.tsx
@@ -86,7 +86,7 @@ function DesktopHeader({selectedYear}: {selectedYear?: number}) {
           <div className="w-full flex flex-col items-end relative">
             <Link href="/">
               <a className="w-20 h-20 absolute -top-5">
-                <ImageWithFallback src={`/bilder/${year}/märke.png`} fallbacksrc={`/bilder/reserv/märke.png`} alt={"NollKIT" + year} layout={"fill"} objectFit={"cover"}/>
+                <ImageWithFallback src={`/bilder/${year}/märke.png`} fallbacksrc={`/bilder/reserv/nollbricka_outline.png`} alt={"NollKIT" + year} layout={"fill"} objectFit={"contain"}/>
               </a>
             </Link>
           </div>
@@ -178,7 +178,7 @@ function MobileHeader({selectedYear}: {selectedYear?: number}) {
 
             <Link href="/">
               <a className="w-20 h-20 absolute top-2 left-12">
-                <ImageWithFallback src={`/bilder/${year}/märke.png`} fallbacksrc={`/bilder/reserv/märke.png`} alt={"NollKIT" + year} layout={"fill"} objectFit={"cover"}/>
+                <ImageWithFallback src={`/bilder/${year}/märke.png`} fallbacksrc={`/bilder/reserv/nollbricka_outline.png`} alt={"NollKIT" + year} layout={"fill"} objectFit={"contain"}/>
               </a>
             </Link>
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,6 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>NollKIT - Mottagningskommittén inom Teknologsektionen Informationsteknik</title>
         <meta name="description" content="Varje år planerar och arrangerar vi mottagningen för de nyantagna studenterna på programmet Informationsteknik på Chalmers." />
-        <link rel="icon" href="/nollbricka.png" />
         <style>
           {`
             @font-face {
